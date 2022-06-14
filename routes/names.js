2588edf7a2fad3e51bb8994f9cc7b84607366da5
alexbelloni <alexbelloni@outlook.com>
2022-06-14 09:23:50 -0400

    env flag for test

--- routes/names.js
+++ routes/names.js
@@ -2,8 +2,11 @@ const controller = require('../controllers/names');
 
 module.exports = (app) => {
     app.post('/names', (req, res) => {
-        controller.read(req.body.group).then(u => res.json(u));
-        //res.json(["Alex","André"])
+        if(process.env.FACEBOOK_ACCESS_DISABLED === 1){
+            res.json(["Alex","André"])
+        } else {
+            controller.read(req.body.group).then(u => res.json(u));
+        }        
     });
 
 }
