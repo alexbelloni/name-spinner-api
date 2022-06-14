d2ae43c6a4bb3ed205a55b25f35a76af0149f1f5
alexbelloni <alexbelloni@outlook.com>
2022-06-14 09:19:14 -0400

    added cors options

--- routes/names.js
+++ routes/names.js
@@ -2,8 +2,8 @@ const controller = require('../controllers/names');
 
 module.exports = (app) => {
     app.post('/names', (req, res) => {
-        //controller.read(req.body.group).then(u => res.json(u));
-        res.json(["Alex","André"])
+        controller.read(req.body.group).then(u => res.json(u));
+        //res.json(["Alex","André"])
     });
 
 }
