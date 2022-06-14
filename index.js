7a48d5f268ebdb84b5b3d9ddd2d578d5251a02d6
alexbelloni <alexbelloni@outlook.com>
2022-06-14 09:31:18 -0400

    no access control allow config outside cors

--- index.js
+++ index.js
@@ -19,12 +19,12 @@ var corsOptions = {
   }
 app.use(cors(corsOptions));
 
-app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PATCH, DELETE');//, OPTIONS');
-    next();
-});
+// app.use(function (req, res, next) {
+//     res.setHeader('Access-Control-Allow-Origin', '*');
+//     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+//     res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PATCH, DELETE');//, OPTIONS');
+//     next();
+// });
 
 app.use(express.json());
 
