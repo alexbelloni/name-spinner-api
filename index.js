cc578db9ced41a1361c57ada2274cc7754b3e38e
alexbelloni <alexbelloni@outlook.com>
2022-06-13 22:46:02 -0400

    cors

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ logger(app);
 app.use(cors());
 
 app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', 'https://fearful-ice.surge.sh/');
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PATCH, DELETE');//, OPTIONS');
     next();
